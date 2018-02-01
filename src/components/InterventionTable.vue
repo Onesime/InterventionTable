1963460b5b769dd3ed75cd67ecedc8ca03d55cfd
deleham <onesime.deleham@gmail.com>
2018-02-01 15:27:33 +0100

    Add navigation

--- src/components/InterventionTable.vue
+++ src/components/InterventionTable.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="interventiontable">
 		<div class="table-responsive">
-			<table class="table table-responsive">
+			<table class="table">
 				<thead>
 					<tr>
 						<th>ID</th>
@@ -10,7 +10,7 @@
 						<th>Email</th>
 						<th>Title</th>
 						<th>Place</th>
-            <th>Action</th>
+            			<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -18,10 +18,13 @@
 						:row="i"
 						:key="index">
 					</Row>
-          <Create :json="json"></Create>
+          			<Create v-if="pageNumber == maxSize" :json="json"></Create>
 				</tbody>
 			</table>
 		</div>
+		<Navigation :maxSize="maxSize"
+					:pageNumber="pageNumber"
+					:pageChanged="pageChanged"></Navigation>
 	</div>
 </template>
 
@@ -30,29 +33,47 @@
 import json from '../json/MOCK_DATA.json'
 import Row from './Row.vue'
 import Create from './Create.vue'
+import Navigation from './Navigation.vue'
 
 export default {
   name: 'InterventionTable',
 	data() {
         return {
-            json: json
+            json: json,
+            pageNumber: 1,
         }
     },
 	props: {
 
 	},
 	computed: {
-      paginatedJson: function() {
-          return json;
-	  }
+      	paginatedJson: function() {
+          	return this.json.slice((this.pageNumber -1) * 10, (this.pageNumber - 1) * 10 + 10);
+	  	},
+        maxSize: function() {
+            return Math.ceil(this.json.length / 10);
+        },
 	},
     components: {
-      Row: Row,
-      Create: Create
-    }
+      	Row: Row,
+      	Create: Create,
+        Navigation: Navigation
+	},
+	methods: {
+      pageChanged: function(index) {
+          if (index < 1) this.pageNumber = 1;
+          else if (index > this.maxSize - 1) this.pageNumber = this.maxSize;
+          else this.pageNumber = index;
+	  }
+	}
 }
 </script>
 
 <style scoped>
+
+	.interventiontable {
+		margin-left: 20%;
+		width: 60%;
+	}
 
 </style>
