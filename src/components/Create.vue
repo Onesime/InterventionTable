97a9feacf95f98efc6871b64246cda5d22aabbad
Nicolas Delage <nicolasdelage7@gmail.com>
2018-02-26 17:10:16 +0100

    fix add intervention

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,12 +1,12 @@
 <template>
   <tr>
     <td></td>
-    <td></td>
-    <td><input type="text" placeholder="first_name" v-model="first_name"/></td>
-    <td><input type="text" placeholder="last_name" v-model="last_name"/></td>
-    <td><input type="text" placeholder="email" v-model="email"/></td>
-    <td><input type="text" placeholder="title" v-model="title"/></td>
-    <td><input type="text" placeholder="place" v-model="place"/></td>
+    <td class="id"></td>
+    <td class="first_name"><input type="text" placeholder="first_name" v-model="first_name"/></td>
+    <td class="last_name"><input type="text" placeholder="last_name" v-model="last_name"/></td>
+    <td class="email"><input type="text" placeholder="email" v-model="email"/></td>
+    <td class="title"><input type="text" placeholder="title" v-model="title"/></td>
+    <td class="place"><input type="text" placeholder="place" v-model="place"/></td>
     <td><input type="button" class="btn btn-default" value="+" id="add" v-on:click="add"/></td>
   </tr>
 </template>
